fb6c1c576bc8dbb28e096ea951ebfb746f2bedd3
Mitchelson Silva <mitchelsonps@gmail.com>
2023-09-14 17:53:34 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mitchelson Silva on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
