6fab263ebb6f904c9dad3bf73990d4886c56e046
Mitchelson Silva <mitchelsonps@gmail.com>
2023-09-14 17:55:40 -0400

    Initial Commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,20 +2,63 @@
 //  ContentView.swift
 //  Memorize
 //
-//  Created by Mitchelson Silva on 14/09/23.
+//  Created by Mitchelson Silva on 12/09/23.
 //
 
 import SwiftUI
 
+
+// ContentView behaves like a View, se comporta como uma View
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack{
+            VStack{
+                CardView()
+                CardView()
+                CardView()
+                CardView()
+            }
+            VStack{
+                CardView()
+                CardView()
+                CardView()
+                CardView()
+            }
+            VStack{
+                CardView()
+                CardView()
+                CardView()
+                CardView()
+            }
+            VStack{
+                CardView()
+                CardView()
+                CardView()
+                CardView()
+            }
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        ZStack (content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 3)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.orange)
+            }
+            
+        })
+        .imageScale(.small)
+        .foregroundColor(.orange)
     }
 }
 
